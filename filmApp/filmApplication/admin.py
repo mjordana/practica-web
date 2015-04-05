7878291a79e9870ae0b5d158b5ba5409593f1b3a
Meritxell Jordana <meritxelljordanagavieiro@gmail.com>
2015-04-05 16:19:00 +0200

    Models i admin completats

--- filmApp/filmApplication/admin.py
+++ filmApp/filmApplication/admin.py
@@ -1,3 +1,9 @@
+import models
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(models.Movie)
+admin.site.register(models.Actor)
+admin.site.register(models.Director)
+admin.site.register(models.MovieReview)
+admin.site.register(models.Genre)
--- filmApp/filmApplication/models.py
+++ filmApp/filmApplication/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Actor(models.Model):
+    name = models.TextField(max_length = 50)
+    birthYear = models.IntegerField()
+    birthPlace = models.TextField(max_length = 50)
+    nationality = models.TextField(max_length = 15)
+
+    def __unicode__(self):
+        return u"%s" % self.name
+    def get_absolute_url(self):
+        return reverse('filmApplication:actor_detail',kwargs={'pk':self.pk})
+
+
+class Director(models.Model):
+    name = models.TextField(max_length = 50)
+    birthYear = models.IntegerField()
+    birthPlace = models.TextField(max_length = 50)
+    nationality = models.TextField(max_length = 15)
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+    def get_absolute_url(self):
+        return reverse('filmApplication:director_detail',kwargs={'pk':self.pk})
+
+
+class Genre(models.Model):
+    tipus = (('comedy','comedy'),('action','action'),('drama','drama'),('terror','terror'),
+        ('fantasy','fantasy'),('thriller','thriller'),('aventura','aventura'),('scienceFiction','scienceFiction'),
+	    ('western','western'))
+    genre = models.CharField(max_length=15,choices=tipus,unique=True)
+
+    def __unicode__(self):
+        return self.genre
+
+class Movie(models.Model):
+    title = models.TextField(max_length = 50)
+    year = models.IntegerField()
+    duration = models.IntegerField()
+    genre = models.ForeignKey(Genre)
+    director = models.ForeignKey(Director)
+    actors = models.ManyToManyField(Actor)
+
+    def __unicode__(self):
+        return u"%s" % self.title
+
+    def get_absolute_url(self):
+        return reverse('filmApplication:film_detail',kwargs={'pk':self.pk})
+
+
+
+class Review(models.Model):
+
+    RATING_CHOICES = ((1,'1'), (2,'2'), (3,'3'), (4,'4'), (5,'5'))
+    rating = models.PositiveSmallIntegerField('Rating (stars)', blank=False, default=3,
+		choices=RATING_CHOICES)
+    comment = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, default=1)
+    date = models.DateField(default=date.today)
+
+    class Meta:
+        abstract = True
+
+
+class MovieReview(models.Model):
+    movie = models.ForeignKey(Movie)
+    def get_absolute_url(self):
+        return reverse('filmApplication:movie_detail',kwargs={'pk':self.movie.pk})
+
+
