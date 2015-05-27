688b1f039d0ee78abafb7edbce1374d3e71a9dec
Meritxell Jordana <meritxelljordanagavieiro@gmail.com>
2015-05-27 13:28:04 +0200

    Fixed an error with a JSON/XML url

--- filmApp/filmApplication/urls.py
+++ filmApp/filmApplication/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    url(r'^directors\.(?P<extension>(json|xml))$',
+    url(r'^directors\.(?P<extension>(json|xml))/$',
         DirectorList.as_view(),
         name='directors_list_conneg'),
 
